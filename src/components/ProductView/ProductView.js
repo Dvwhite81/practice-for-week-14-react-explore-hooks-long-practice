84a50b284b2980c33eb7fc074db35c14f685cde5
Dvwhite81 <Dvwhite1981@gmail.com>
2023-08-30 11:46:24 -0400

    Complete

--- src/components/ProductView/ProductView.js
+++ src/components/ProductView/ProductView.js
@@ -1,35 +1,52 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
 import ProductListItem from "../ProductListItem";
 import ProductDetails from "../ProductDetails";
-import './ProductView.css'
+import "./ProductView.css";
 
 function ProductView({ products }) {
+  // Get local storage - side panel
+  const getSavedPanel = localStorage.getItem('sideOpen');
+  const savedPanel = getSavedPanel === 'false' ? false: true;
+  const [sideOpen, setSideOpen] = useState(savedPanel);
+  const [selectedProduct, setSelectedProduct] = useState();
 
-  // TODO: Replace with state variable
-  const sideOpen = true;
+  useEffect(() => {
+    if (selectedProduct) setSideOpen(true);
+  }, [selectedProduct]);
+
+  useEffect(() => {
+    // Set local storage
+    localStorage.setItem('sideOpen', sideOpen);
+    if (!sideOpen) setSelectedProduct();
+  }, [sideOpen]);
 
   return (
     <div className="product-view">
       <div className="product-main-area">
         <h1>Products</h1>
         <div className="product-list">
-          {products.map(item =>
+          {products.map((item) => (
             <ProductListItem
               key={item.id}
               product={item}
-              onClick={() => console.log('SELECT PRODUCT', item)}
+              isSelected={
+                selectedProduct ? selectedProduct.id === item.id : false
+              }
+              onClick={() => setSelectedProduct(item)}
             />
-          )}
+          ))}
         </div>
       </div>
       <div className="product-side-panel">
         <div className="product-side-panel-toggle-wrapper">
-          <div className="product-side-panel-toggle"
-               onClick={() => console.log('TOGGLE SIDE PANEL')}>
-            {sideOpen ? '>' : '<'}
+          <div
+            className="product-side-panel-toggle"
+            onClick={() => setSideOpen(!sideOpen)}
+          >
+            {sideOpen ? ">" : "<"}
           </div>
         </div>
-        <ProductDetails visible={sideOpen} />
+        <ProductDetails visible={sideOpen} product={selectedProduct} />
       </div>
     </div>
   );
